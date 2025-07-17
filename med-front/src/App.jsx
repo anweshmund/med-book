3c9087ff42a18beb832661c9cd5bbbdf23ce095d
anwesh <anweshmund25@gmail.com>
2025-07-17 23:13:19 +0530

    initial commit

---
+++ med-front/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+
+import Login from './pages/login'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Profile from './pages/profile'
+import Mybook from './pages/mybook'
+import Appointment from './pages/Appointment'
+import Navbar from './component/Navbar'
+import Doctor from './pages/Doctor'
+
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]' >
+      <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/doctors' element={<Doctor/>} />
+      <Route path='/doctors/:speciality' element={<Doctor/>} />
+      
+      <Route part='/login' element ={<Login/>}/>
+      <Route part='/about' element ={<About/>}/>
+      <Route part='/contact' element ={<Contact/>}/>
+      <Route part='/my-profile' element ={<Profile/>}/>
+      <Route part='/my-appointments' element ={<Mybook/>}/>
+      <Route part='/appointments:/docId' element ={<Appointment/>}/>
+    </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ med-front/src/pages/Mybook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Mybook = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Mybook
