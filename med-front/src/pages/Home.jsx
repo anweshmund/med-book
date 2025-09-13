ddfd1b91369062c408dd8a6b7464724285b8d826
anwesh <anweshmund25@gmail.com>
2025-09-13 18:20:19 +0530

    spec doc

--- med-front/src/pages/Home.jsx
+++ med-front/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../component/Header'
+import SpecialityMenu from '../component/SpecialityMenu'
 
 const Home = () => {
   return (
     <div>
      <Header/>
+     <SpecialityMenu/>
     </div>
   )
 }
